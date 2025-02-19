7401f5956d9c8b1e5fec6a35a3e8d4a765fce80b
CodingWithMK <032180115@ogr.uludag.edu.tr>
2025-02-19 10:50:54 +0100

    feat: System Hardware Usage Monitor added.

---
+++ concepts/system_info_monitor.py
@@ -0,0 +1,58 @@
+import psutil
+import tkinter as tk
+import ttkbootstrap
+import os
+import sys
+from gpiozero import CPUTemperature
+import clr
+
+class SystemInformationMonitor(ttkbootstrap.Window):
+    def __init__(self):
+        super().__init__()
+
+        self.openhardwaremonitor_hwtypes = ["Mainboard", "SuperIO", "CPU", "RAM", "GpuNvidia", "GpuAti", "TBalancer", "Heatmaster", "HDD"]
+        self.cputhermometer_hwtypes = ["Mainboard", "SuperIO", "CPU", "GpuNvidia", "GpuAti", "TBalancer", "Heatmaster", "HDD"]
+        self.openhardwaremonitor_sensortypes = ["Voltage", "Clock", "Temperature", "Load", "Fan", "Flow", "Control", "Level", "Factor", "Power", "Data", "SmallData"]
+        self.openhardwaremonitor_sensortypes = ["Voltage", "Clock", "Temperature", "Load", "Fan", "Flow", "Control", "Level"]
+        
+        self.title("System Information Monitor")
+        self.geometry("800x600")
+
+        self.cpu_usage_label = ttkbootstrap.Label(self, text="CPU Usage: 0%", font=("Arial", 12))
+        self.cpu_usage_label.grid(row=0, column=0, padx=10, pady=10)
+
+        self.cpu_meter = ttkbootstrap.Meter(self, metersize=120, amountused=0, padding=10, metertype="full", interactive=False)
+        self.cpu_meter.grid(row=1, column=1, padx=10, pady=10)
+
+        self.cpu_temperature_label = ttkbootstrap.Label(self, text="CPU Temperature: 0°C", font=("Arial", 12))
+        self.cpu_temperature_label.grid(row=0, column=1, padx=10, pady=10)
+
+        self.memory_usage_label = ttkbootstrap.Label(self, text="RAM Usage: 0%", font=("Arial", 12))
+        self.memory_usage_label.grid(row=1, column=0, padx=10, pady=10)
+
+        self.memory_meter = ttkbootstrap.Meter(self, metersize=120, amountused=0, padding=10, metertype="full", interactive=False)
+        self.memory_meter.grid(row=2, column=1, padx=10, pady=10)
+
+        self.disk_usage_label = ttkbootstrap.Label(self, text="Disk Usage: 0%", font=("Arial", 12))
+        self.disk_usage_label.grid(row=2, column=0, padx=10, pady=10)
+
+        self.update_system_usage()
+
+    def init_openhardwaremonitor(self):
+        file = ""
+
+    def update_system_usage(self):
+        cpu_usage = psutil.cpu_percent()
+        memory_usage = psutil.virtual_memory().percent
+
+        self.cpu_usage_label.config(text=f"CPU Usage: {cpu_usage:.2f}%")
+        self.memory_usage_label.config(text=f"RAM Usage: {memory_usage:.2f}%")
+
+        self.after(1000, self.update_system_usage)
+
+if __name__ == "__main__":
+    window = SystemInformationMonitor()
+    window.mainloop()
+
+        
+
